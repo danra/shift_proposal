127307f1aa570a22770eca298dbda7984298c80f
Dan Raviv <dan@soundradix.com>
2017-08-10 11:24:31 +0300

    Use correct type for shift count argument and std::prev instead of minus to support all bidirectional iterators

--- shift_proposal.h
+++ shift_proposal.h
@@ -1,18 +1,19 @@
 #pragma once
 
 #include <algorithm>
+#include <iterator>
 
 template<class BidirIt> // TODO: shifting elements left should also be allowed for forward iterators
-void shift(BidirIt first, BidirIt last, int n)
+void shift(BidirIt first, BidirIt last, typename std::iterator_traits<BidirIt>::difference_type n)
 {
     if (n == 0)
         return;
 
     // Shift elements right (forward)
     if (n > 0)
-        std::move_backward(first, last - n, last);
+        std::move_backward(first, std::prev(last, n), last);
     
     // Shift elements left (backward)
     else
-        std::move(first - n, last, first);
+        std::move(std::prev(first, n), last, first);
 }
