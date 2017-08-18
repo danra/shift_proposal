13e5b950439f810236922c4c19a6f4368aa1f2a4
Dan Raviv <dan@soundradix.com>
2017-08-19 00:25:26 +0300

    Added optional filler value

--- shift_proposal.h
+++ shift_proposal.h
@@ -4,39 +4,54 @@
 #include <algorithm>
 #include <cassert>
 #include <iterator>
+#include <optional>
 #include <utility>
 
 template<class BidirIt>
-std::pair<BidirIt, BidirIt> shift(BidirIt first, BidirIt last, typename std::iterator_traits<BidirIt>::difference_type n)
+std::pair<BidirIt, BidirIt> shift(BidirIt first, BidirIt last, typename std::iterator_traits<BidirIt>::difference_type n, std::optional<typename std::iterator_traits<BidirIt>::value_type> filler = std::nullopt)
 {
     if (n == 0)
         return { first, last };
 
     // Shift elements right (forward)
     if (n > 0)
-        return { std::move_backward(first, std::prev(last, n), last), last };
-
+    {
+        auto shifted_first = std::move_backward(first, std::prev(last, n), last);
+        if (filler.has_value())
+            std::fill(first, shifted_first, *filler);
+        return { shifted_first, last };
+    }
+    
     // n < 0
     // Shift elements left (backward)
-    return { first, std::move(std::next(first, -n), last, first) };
+    auto shifted_last = std::move(std::next(first, -n), last, first);
+    if (filler.has_value())
+        std::fill(shifted_last, last, *filler);
+    return { first, shifted_last };
 }
 
 template<class ForwardIt>
-ForwardIt shift_left(ForwardIt first, ForwardIt last, typename std::iterator_traits<ForwardIt>::difference_type n)
+ForwardIt shift_left(ForwardIt first, ForwardIt last, typename std::iterator_traits<ForwardIt>::difference_type n, std::optional<typename std::iterator_traits<ForwardIt>::value_type> filler = std::nullopt)
 {
     assert(n >= 0);
     if (n == 0)
         return last;
-    return std::move(std::next(first, n), last, first);
+    auto shifted_last = std::move(std::next(first, n), last, first);
+    if (filler.has_value())
+        std::fill(shifted_last, last, *filler);
+    return shifted_last;
 }
 
 template<class BidirIt>
-BidirIt shift_right(BidirIt first, BidirIt last, typename std::iterator_traits<BidirIt>::difference_type n)
+BidirIt shift_right(BidirIt first, BidirIt last, typename std::iterator_traits<BidirIt>::difference_type n, std::optional<typename std::iterator_traits<BidirIt>::value_type> filler = std::nullopt)
 {
     assert(n >= 0);
     if (n == 0)
         return first;
-    return std::move_backward(first, std::prev(last, n), last);
+    auto shifted_first = std::move_backward(first, std::prev(last, n), last);
+    if (filler.has_value())
+        std::fill(first, shifted_first, *filler);
+    return shifted_first;
 }
 
 #endif // !defined(SHIFT_PROPOSAL_H)
--- test.cpp
+++ test.cpp
@@ -12,11 +12,25 @@ int main()
         assert((v == std::vector<int>{1, 2, 1, 2, 3}));
         assert((std::vector<int>{first, last} == std::vector<int>{1, 2, 3}));
     }
+    
+    {
+        std::vector<int> v {1, 2, 3, 4, 5};
+        auto [first, last] = shift(v.begin(), v.end(), 2, 0);
+        assert((v == std::vector<int>{0, 0, 1, 2, 3}));
+        assert((std::vector<int>{first, last} == std::vector<int>{1, 2, 3}));
+    }
 
     {
         std::vector<int> v {1, 2, 3, 4, 5};
         auto [first, last] = shift(v.begin(), v.end(), -3);
         assert((v == std::vector<int>{4, 5, 3, 4, 5}));
+        assert((std::vector<int>{first, last} == std::vector<int>{4, 5}));
+    }
+    
+    {
+        std::vector<int> v {1, 2, 3, 4, 5};
+        auto [first, last] = shift(v.begin(), v.end(), -3, 0);
+        assert((v == std::vector<int>{4, 5, 0, 0, 0}));
         assert((std::vector<int>{first, last} == std::vector<int>{4, 5}));
     }
     
@@ -28,9 +42,23 @@ int main()
     }
     
     {
+        std::forward_list<int> flist {1, 2, 3, 4, 5};
+        auto last = shift_left(flist.begin(), flist.end(), 3, 0);
+        assert((flist == std::forward_list<int>{4, 5, 0, 0, 0}));
+        assert((std::forward_list<int>{flist.begin(), last} == std::forward_list<int>{4, 5}));
+    }
+    
+    {
         std::vector<int> v {1, 2, 3, 4, 5};
         auto first = shift_right(v.begin(), v.end(), 2);
         assert((v == std::vector<int>{1, 2, 1, 2, 3}));
+        assert((std::vector<int>{first, v.end()} == std::vector<int>{1, 2, 3}));
+    }
+    
+    {
+        std::vector<int> v {1, 2, 3, 4, 5};
+        auto first = shift_right(v.begin(), v.end(), 2, 0);
+        assert((v == std::vector<int>{0, 0, 1, 2, 3}));
         assert((std::vector<int>{first, v.end()} == std::vector<int>{1, 2, 3}));
     }
     
