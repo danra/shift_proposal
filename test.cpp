c435669cc3acbd9a2c807ec6a801592ee570232d
Dan Raviv <dan@soundradix.com>
2017-08-19 01:43:12 +0300

    Removed general shift in favor of specific shift_left and shift_right

--- test.cpp
+++ test.cpp
@@ -7,34 +7,6 @@
 int main()
 {
     {
-        std::vector<int> v {1, 2, 3, 4, 5};
-        auto [first, last] = shift(v.begin(), v.end(), 2);
-        assert((v == std::vector<int>{1, 2, 1, 2, 3}));
-        assert((std::vector<int>{first, last} == std::vector<int>{1, 2, 3}));
-    }
-    
-    {
-        std::vector<int> v {1, 2, 3, 4, 5};
-        auto [first, last] = shift(v.begin(), v.end(), 2, 0);
-        assert((v == std::vector<int>{0, 0, 1, 2, 3}));
-        assert((std::vector<int>{first, last} == std::vector<int>{1, 2, 3}));
-    }
-
-    {
-        std::vector<int> v {1, 2, 3, 4, 5};
-        auto [first, last] = shift(v.begin(), v.end(), -3);
-        assert((v == std::vector<int>{4, 5, 3, 4, 5}));
-        assert((std::vector<int>{first, last} == std::vector<int>{4, 5}));
-    }
-    
-    {
-        std::vector<int> v {1, 2, 3, 4, 5};
-        auto [first, last] = shift(v.begin(), v.end(), -3, 0);
-        assert((v == std::vector<int>{4, 5, 0, 0, 0}));
-        assert((std::vector<int>{first, last} == std::vector<int>{4, 5}));
-    }
-    
-    {
         std::forward_list<int> flist {1, 2, 3, 4, 5};
         auto last = shift_left(flist.begin(), flist.end(), 3);
         assert((flist == std::forward_list<int>{4, 5, 3, 4, 5}));
