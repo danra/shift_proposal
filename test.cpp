2cdefce10326f65349f601cf86a6790c9fb1459f
Dan Raviv <dan@soundradix.com>
2017-08-24 23:36:29 +0300

    Fix generate_indexes for 0 size (generated 1-long sequences of 0 instead of empty sequences for all sequence types except std::array) and add test for empty sequences

--- test.cpp
+++ test.cpp
@@ -9,10 +9,16 @@
 
 namespace {
 
+#ifdef __clang__
+// Tell clang to shut up about std::array {} initializations.
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wmissing-braces"
+#endif // __clang__
+
     namespace detail {
         template<typename C, std::size_t... Is>
         constexpr auto generate_container_with_index_sequence(std::index_sequence<Is...>) -> C {
-            return {{std::integral_constant<std::size_t, Is>{}...}};
+            return {std::integral_constant<std::size_t, Is>{}...};
         }
     } // namespace detail
     
@@ -28,19 +34,19 @@ namespace {
 
         const auto control = generate_indexes<std::array<int, n>, n>();
         if constexpr (n == 5) {
-            assert((control == std::array<int, 5> {{0, 1, 2, 3, 4}}));
+            assert((control == std::array<int, 5> {0, 1, 2, 3, 4}));
         }
         
-        // shift left by i in [-2n, 3n)
-        for (auto i = -2 * n; i < 3 * n; ++i) {
+        // shift left by i in [-2n-2, 3n+3)
+        for (auto i = -2 * n - 2; i < 3 * n + 3; ++i) {
             auto const clamped = std::clamp(i, 0, n);
             auto s = generate_indexes<Sequence, n>();
             auto const last = shift_left(begin(s), end(s), i);
             assert(std::equal(begin(control) + clamped, end(control), begin(s), last));
         }
 
-        // shift left by i in [-2n, 3n) and fill
-        for (auto i = -2 * n; i < 3 * n; ++i) {
+        // shift left by i in [-2n-2, 3n+3) and fill
+        for (auto i = -2 * n - 2; i < 3 * n + 3; ++i) {
             auto const clamped = std::clamp(i, 0, n);
             auto s = generate_indexes<Sequence, n>();
             auto const last = shift_left(begin(s), end(s), i, 0);
@@ -48,16 +54,16 @@ namespace {
             assert(std::all_of(last, end(s), [](auto const& x){ return x == 0; }));
         }
         
-        // shift right by i in [-2n, 3n)
-        for (auto i = -2 * n; i < 3 * n; ++i) {
+        // shift right by i in [-2n-2, 3n+3)
+        for (auto i = -2 * n - 2; i < 3 * n + 3; ++i) {
             auto const clamped = std::clamp(i, 0, n);
             auto s = generate_indexes<Sequence, n>();
             auto const first = shift_right(begin(s), end(s), i);
             assert(std::equal(begin(control), end(control) - clamped, first, end(s)));
         }
         
-        // shift right by i in [-2n, 3n) and fill
-        for (auto i = -2 * n; i < 3 * n; ++i) {
+        // shift right by i in [-2n-2, 3n+3) and fill
+        for (auto i = -2 * n - 2; i < 3 * n + 3; ++i) {
             auto const clamped = std::clamp(i, 0, n);
             auto s = generate_indexes<Sequence, n>();
             auto const first = shift_right(begin(s), end(s), i, 0);
@@ -75,11 +81,16 @@ namespace {
         test<std::list<int>, n>();
         test<std::vector<int>, n>();
     }
-    
+
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif // __clang__
+
 } // unnamed namespace
 
 int main()
 {
+    testAll<0>();
     testAll<1>();
     testAll<2>();
     testAll<3>();
